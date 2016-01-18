daf396f17cd9db60dad9c24b2572ad8db32977d9
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-01-18 16:17:37 +0100

    integrating parser in scala infrastructure

---
+++ src/main/scala/eu/nomad_lab/parsers/CastepParser.scala
@@ -0,0 +1,26 @@
+package eu.nomad_lab.parsers
+import eu.nomad_lab.DefaultPythonInterpreter
+import org.{json4s => jn}
+
+object CastepParser extends SimpleExternalParserGenerator(
+  name = "CastepParser",
+  parserInfo = jn.JObject(
+    ("name" -> jn.JString("CastepParser")) ::
+      ("version" -> jn.JString("1.0")) :: Nil),
+  mainFileTypes = Seq("text/.*"),
+  mainFileRe = """\s\|\s*CCC\s*AA\s*SSS\s*TTTTT\s*EEEEE\s*PPPP\s*\|\s*""".r,
+  cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/castep/parser/parser-castep/SimpleCastepParser.py",
+    "--uri", "${mainFileUri}", "${mainFilePath}"),
+  resList = Seq(
+    "parser-castep/SimpleCastepParser.py",
+    "parser-castep/CastepBandParser.py",
+    "parser-castep/setup_paths.py",
+    "nomad_meta_info/common.nomadmetainfo.json",
+    "nomad_meta_info/meta_types.nomadmetainfo.json",
+    "nomad_meta_info/castep.nomadmetainfo.json"
+  ) ++ DefaultPythonInterpreter.commonFiles(),
+  dirMap = Map(
+    "parser-castep" -> "parsers/castep/parser/parser-castep",
+    "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info"
+  ) ++ DefaultPythonInterpreter.commonDirMapping()
+)
