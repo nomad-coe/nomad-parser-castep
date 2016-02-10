fc878cb601d04932800f802fff4790a8411f8dfe
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-02-10 15:14:11 +0100

    update test

--- src/test/scala/eu/nomad_lab/parsers/CastepParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/CastepParserSpec.scala
@@ -6,7 +6,7 @@ import org.specs2.mutable.Specification
 
 object CastepParserSpec extends Specification {
   "CastepParserTest" >> {
-    examplesBlock {
+    "test with Si2" >>  {
       ParserRun.parse(CastepParser,"parsers/castep/test/examples/Si2.castep","json-events") must_== ParseResult.ParseSuccess
     }
   }
