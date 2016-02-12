0831f53a48b6826d8f64360d7f193b08eb0f069b
Riello, Massimo (mriello) <massimo.riello@kcl.ac.uk>
2016-02-12 12:31:14 +0100

    Updating the test file's name

--- src/test/scala/eu/nomad_lab/parsers/CastepParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/CastepParserSpec.scala
@@ -7,10 +7,10 @@ import org.specs2.mutable.Specification
 object CastepParserSpec extends Specification {
   "CastepParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(CastepParser, "parsers/castep/test/examples/Si2.castep", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(CastepParser, "parsers/castep/test/examples/Si2.castep_v_1", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(CastepParser, "parsers/castep/test/examples/Si2.castep", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(CastepParser, "parsers/castep/test/examples/Si2.castep_v_1", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
