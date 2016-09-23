b88799d0d05f3f04859493b0b5462b0bb5779920
martina stella <martinastella@ip-036-010.vpn.soton.ac.uk>
2016-09-23 15:56:48 +0100

    bug

--- src/test/scala/eu/nomad_lab/parsers/CastepParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/CastepParserSpec.scala
@@ -176,3 +176,15 @@ object CastepParserSpec16 extends Specification {
     }
   }
 }
+object CastepParserSpec17 extends Specification {
+  "CastepParserTest" >> {
+    "test with json-events" >> {
+      ParserRun.parse(CastepParser, "parsers/castep/test/examples/CH-boatbands.castep", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test with json" >> {
+      ParserRun.parse(CastepParser, "parsers/castep/test/examples/CH-boatbands.castep", "json") must_== ParseResult.ParseSuccess
+    }
+  }
+}
+
+
